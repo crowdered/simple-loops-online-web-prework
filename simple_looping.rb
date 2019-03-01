990d0d2f892c2f83945cc4b91cbba6944903bb9b
Mark Crowder <crowdered@gmail.com>
2019-03-01 16:35:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,8 +25,8 @@ end
 
 def times_iterator(number_of_times)
  7.times.do
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  puts = "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def while_iterator(number_of_times)
