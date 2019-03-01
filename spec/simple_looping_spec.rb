06d4befc34de1088aa85058ad6c50ff90c2d4ceb
Mark Crowder <crowdered@gmail.com>
2019-03-01 16:43:47 +0000

    Automatically backed up by Learn

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -10,8 +10,6 @@ describe 'Loops' do
       expect{ loop_iterator(7) }.to output(@looping_string).to_stdout
     end
   end
-  
-end
 
   describe 'times' do
     it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
